78d899e26bbb08bd45dcef3e7da3e612831c452c
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-07 19:47:13 -0700

    update self and update quality functions

--- idea.js
+++ idea.js
@@ -3,6 +3,19 @@ class Idea{
     this.body = body;
     this.id = id;
     this.title = title;
-    this.quality = quality || 'swill';
+    this.quality = quality || 0;
+  }
+
+  updateSelf(name, text) {
+    this[name] = text
+  }
+
+  updateQuality(vote, length) {
+    if(vote ==='up' && this.quality < length-1) {
+      console.log('hey')
+      this.quality++
+    } else if ( vote === 'down' && this.quality > 0) {
+      this.quality--
+    }
   }
 }
